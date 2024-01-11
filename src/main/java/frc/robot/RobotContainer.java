cdf071f2767b6b43c4027645c75125d1a3b80219
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2024-01-10 19:23:37 -0800

    fixed arm command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,7 @@ package frc.robot;
 
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.DrivetrainSubsystem;
-import frc.robot.subsystems.Commands.ArmCommand;
+import frc.robot.commands.ArmCommand;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
