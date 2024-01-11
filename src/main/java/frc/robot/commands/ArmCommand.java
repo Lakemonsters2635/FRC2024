e656ff50161aa5301ae113f0ae25f72b8e84b987
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2024-01-10 18:17:09 -0800

    Compile error resolved

--- src/main/java/frc/robot/commands/ArmCommand.java
+++ src/main/java/frc/robot/commands/ArmCommand.java
@@ -5,38 +5,24 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.ArmSubsystem;
 
 public class ArmCommand extends Command {
   /** Creates a new ArmCommand. */
-  private ArmSubsystem m_armSubsystem;
-
-
-  public ArmCommand(ArmSubsystem armSubsystem) {
-    m_armSubsystem = armSubsystem;
-
-    addRequirements(m_armSubsystem);
+  public ArmCommand() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-    m_armSubsystem.ArmStart();
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
-  
   @Override
-  public void end(boolean interrupted) {
-    m_armSubsystem.ArmStop();
-
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
